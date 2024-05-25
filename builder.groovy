f91d49f504730ee9846b442ec0ae1e6a20a48c99
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-05-26 01:33:18 +0530

    Update builder.groovy

--- builder.groovy
+++ builder.groovy
@@ -1,33 +1,28 @@
 import groovy.json.JsonSlurper
 
-def deploy(inputfile){
+def deploy(inputFile) {
+    try {
+        // Read inputs from JSON file
+        def input = new JsonSlurper().parse(new File(inputFile))
 
-    // Read inputs from JSON file
-    def input = new JsonSlurper().parse(new File(inputFile))
+        def dockerImage = input.docker.image
+        def dockerTag = input.docker.tag
+        def containerName = input.docker.container_name
 
-    def dockerimage = input.docker.image
-    def dockerTag = input.docker.tag
-    def containerName = input.docker.container_name
+        // Delete existing container if available
+        sh "docker rm -f ${containerName}"
 
-    // Delete existing container if available 
+        echo "Deleting container ${containerName}"
 
-    sh 'docker rm -f ${containerName}'
+        // Build Docker image
+        sh "docker build -t ${dockerImage}:${dockerTag} ."
 
-    echo 'Deleting container ${containerName}'
-    
-
-    // Build Docker image
-
-    sh 'docker build -t ${dockerimage}:${dockertag} .'
-
-    // Run Docker container
-    sh """
-    docker run  --rm -d --name ${containerName} -p 80:8080  ${dockerImage}:${dockerTag}
-    """
-
-
-
-
-
-
+        // Run Docker container
+        sh """
+        docker run --rm -d --name ${containerName} -p 80:8080 ${dockerImage}:${dockerTag}
+        """
+    } catch (Exception e) {
+        println "Error: ${e.message}"
+        throw e
+    }
 }
