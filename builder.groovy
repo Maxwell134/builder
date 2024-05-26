1bc5800197a4fa51d8b53c312b8a4e6181416d52
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-05-27 00:34:56 +0530

    Update builder.groovy

--- builder.groovy
+++ builder.groovy
@@ -1,30 +1,13 @@
-import groovy.json.JsonSlurper
-
-def deploy(inputFile) {
-    try {
-        // Read inputs from JSON file
-        def input = new JsonSlurper().parse(new File(inputFile))
-
-        def dockerImage = input.docker.image
-        def dockerTag = input.docker.tag
-        def containerName = input.docker.container_name
-
-        // Delete existing container if available
-        sh "docker rm -f ${containerName}"
-
-        echo "Deleting container ${containerName}"
-
-        // Build Docker image
-        sh "docker build -t ${dockerImage}:${dockerTag} ."
-
-        // Run Docker container
-        sh """
-        docker run --rm -d --name ${containerName} -p 80:8080 ${dockerImage}:${dockerTag}
-        """
-    } catch (Exception e) {
-        println "Error: ${e.message}"
-        throw e
-    }
+def buildDockerImage(String imageName, String buildNumber) {
+    sh "docker build -t ${imageName}:${buildNumber} ."
 }
 
-return  this 
+
+def runDockerContainer(String imageName, String buildNumber, String port) {
+    def containerName = "${imageName.replace('/', '_')}-${buildNumber}"
+    sh "docker stop ${containerName} || true"
+    sh "docker rm ${containerName} || true"
+    sh "docker run -d --name ${containerName} -p ${port}:${port} ${imageName}:${buildNumber}"
+}
+
+return this
